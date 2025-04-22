2dfae29c66528f59680ba754358672a8e40fe275
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2025-04-22 11:28:37 -0300

    mexi numas coisas aí sei lá

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, jsonify
 
 app = Flask(__name__)
 
+salaSituation = ""
+
 @app.route('/')
-# @app.route("/dashboard")
+@app.route("/dashboard")
 def pagina_dashboard():
     
     return render_template("index.html")
 
-@app.route("/lampada/ligada")
+@app.route("/lampada/luzEstado")
 def post_lampada_ligada():
     lightResult = "desligada"
     
