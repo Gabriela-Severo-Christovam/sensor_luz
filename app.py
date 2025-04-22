c388a450c74b44288149e86337c5b22a125cb9f1
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2025-04-22 10:03:56 -0300

    rota de página inicial, definição do app.run e import do flask

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask 
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route("/dashboard")
+# @app.route("/dashboard")
 def pagina_dashboard():
-    return "pagina em construção"
+    
+    return render_template("index.html")
 
 @app.route("/lampada/ligada")
 def post_lampada_ligada():
     return "pagina em construção"
+
+app.run(host="0.0.0.0", port=8080)
