0bd792c235bfc1c19088469854f48db9e3c88629
Gabriela <severogabriela4@gmail.com>
2025-04-29 10:04:07 -0300

    construindo js

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    const resultado = document.getElementById('result');
+
+    const response = await fetch('https://sensor_luz.onrender.com/get/estado_lampada');
+    const estado_luz = await response.json();
+
+    const estadoLuz = estado_luz.value; 
+    resultado.innerHTML = `<span>${estadoLuz}</span>`;
+});
