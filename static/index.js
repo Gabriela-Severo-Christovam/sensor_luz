5603bcadbac03ef636a2d24dcb708ebd43dd48ea
Gabriela <severogabriela4@gmail.com>
2025-05-06 11:22:09 -0300

    correção JS

--- static/index.js
+++ static/index.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', async function() {
     const response = await fetch('https://projeto-sensor-luz.onrender.com/get/estado_lampada');
     const estado_luz = await response.json();
 
-    const estadoLuz = estado_luz.situacao_sala; 
+    const estadoLuz = estado_luz.value; 
     resultado.innerHTML = `<span>${estadoLuz}</span>`;
 });
