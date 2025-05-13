3611b94f80ffb022e0c9de403c9db406c899c5b7
DavidArrudaSaches <david.sanches2750@gmail.com>
2025-05-13 08:01:52 -0300

    Atualização Final

--- static/index.js
+++ static/index.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', async function() {
     const response = await fetch('https://projeto-sensor-luz.onrender.com/get/estado_lampada');
     const estado_luz = await response.json();
 
-    const estadoLuz = estado_luz.value; 
+    const estadoLuz = estado_luz.ESTADO_LAMPADA; 
     resultado.innerHTML = `<span>${estadoLuz}</span>`;
 });
